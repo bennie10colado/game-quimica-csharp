d93764287c91a5dd27e5fbaaa800077a3e7f43d5
Benjamin <86573978+bennie10colado@users.noreply.github.com>
2023-08-28 02:49:24 -0300

    adding more solubility tests

--- ChemistryLab/Assets/Scripts/GameControllerTwo.cs
+++ ChemistryLab/Assets/Scripts/GameControllerTwo.cs
@@ -5,38 +5,63 @@ using UnityEngine;
 public class GameControllerTwo : MonoBehaviour
 {
     [SerializeField] private GameObject waterObject;
-    [SerializeField] private GameObject etanoatoObject;
+    [SerializeField] private GameObject eterObject;
 
-    private SubstanceSolvent aguaDestilada;
+    [SerializeField] private GameObject etanoatoObject;
+    [SerializeField] private GameObject propanoicAcidObject;
+    [SerializeField] private GameObject butanaminaObject;
+
+    private SubstanceSolvent distilledWater;
+    private SubstanceSolvent eterDietilico;
+
     private SubstanceCompound etanoato;
+    private SubstanceCompound propanoicAcid;
+    private SubstanceCompound butanamina;
 
     private void Start()
     {
-        aguaDestilada = waterObject.GetComponent<SubstanceSolvent>();
+        distilledWater = waterObject.GetComponent<SubstanceSolvent>();
+        eterDietilico = eterObject.GetComponent<SubstanceSolvent>();
 
-        if (aguaDestilada == null)
+        if (distilledWater == null || eterDietilico == null)
         {
-            Debug.LogError("Componente SubstanceSolvent não encontrado no objeto da água destilada!");
+            Debug.LogError("Componente de 'SubstanceSolvent' não foi encontrado em algum dos objetos!");
+            return;
         }
 
         etanoato = etanoatoObject.GetComponent<SubstanceCompound>();
+        propanoicAcid = propanoicAcidObject.GetComponent<SubstanceCompound>();
+        butanamina = butanaminaObject.GetComponent<SubstanceCompound>();
 
-        if (etanoato == null)
+
+		if (etanoato == null || propanoicAcid == null || butanamina == null)
         {
-            Debug.LogError("Componente SubstanceCompound não encontrado no objeto do etanoato!");
+            Debug.LogError("Componente de 'SubstanceCompound' não encontrado em algum dos objetos!");
+            return;
         }
 
-        aguaDestilada.SetSolubility(etanoato, true);
+        distilledWater.SetSolubility(etanoato, true);
+        distilledWater.SetSolubility(propanoicAcid, true);
+        distilledWater.SetSolubility(butanamina, true);
 
-        bool isEtanoatoSoluble = aguaDestilada.IsSoluble(etanoato);
+        eterDietilico.SetSolubility(etanoato, true);
+        eterDietilico.SetSolubility(propanoicAcid, true);
+        eterDietilico.SetSolubility(butanamina, true);
 
-        if (isEtanoatoSoluble)
-        {
-            Debug.Log("O etanoato é solúvel na água.");
-        }
-        else
-        {
-            Debug.Log("O etanoato não é solúvel na água.");
-        }
+        bool isEtanoatoSolubleInWater = distilledWater.IsSoluble(etanoato);
+        bool isPropanoicAcidSolubleInWater = distilledWater.IsSoluble(propanoicAcid);
+        bool isButanaminaSolubleInWater = distilledWater.IsSoluble(butanamina);
+
+        bool isEtanoatoSolubleInEter = eterDietilico.IsSoluble(etanoato);
+        bool isPropanoicAcidSolubleInEter = eterDietilico.IsSoluble(propanoicAcid);
+        bool isButanaminaSolubleInEter = eterDietilico.IsSoluble(butanamina);
+
+        Debug.Log("Etanoato é solúvel em água: " + isEtanoatoSolubleInWater);
+        Debug.Log("Ácido propanoico é solúvel em água: " + isPropanoicAcidSolubleInWater);
+        Debug.Log("Butan-1-amina é solúvel em água: " + isButanaminaSolubleInWater);
+
+        Debug.Log("Etanoato é solúvel em éter: " + isEtanoatoSolubleInEter);
+        Debug.Log("Ácido propanoico é solúvel em éter: " + isPropanoicAcidSolubleInEter);
+        Debug.Log("Butan-1-amina é solúvel em éter: " + isButanaminaSolubleInEter);
     }
 }
