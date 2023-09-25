10c5eb861b33ab3e7c5aaaf7e7abe29215e2b47d
Benjamin <86573978+bennie10colado@users.noreply.github.com>
2023-09-25 01:53:56 -0300

    remodeling classes for new architecture based on relational database

--- ChemistryLab/Assets/Scripts/JsonScripts/SubstanceManager.cs
+++ ChemistryLab/Assets/Scripts/JsonScripts/SubstanceManager.cs
@@ -4,133 +4,51 @@ using System.IO;
 using System;
 
 [System.Serializable]
-public class CompoundsCollection
+public class ChemicalsCollection
 {
-    public List<CompoundData> compounds;
+    public List<ChemicalCompound> chemicals;
 }
 
 [System.Serializable]
 public class SolventsCollection
 {
-    public List<SolventData> solvents;
+    public List<Solvent> solvents;
+}
+
+[System.Serializable]
+public class SolutionsCollection
+{
+    public List<Solution> solutions;
 }
 
 public class SubstanceManager : MonoBehaviour
 {
-    public string compoundsJsonFilePath = "Assets/Resources/SolubilityData.json";
+    public string solutionsJsonFilePath = "Assets/Resources/SolubilityData.json";
     public string solventsJsonFilePath = "Assets/Resources/SolventsData.json";
+    public string compoundsJsonFilePath = "Assets/Resources/CompoundsData.json";
+    
 
-    private Dictionary<string, SubstanceSolvent> solvents = new Dictionary<string, SubstanceSolvent>();
-    private List<SubstanceCompound> substanceCompounds = new List<SubstanceCompound>();
 
     void Start()
     {
         LoadSolvents();
-        LoadChemicalData();
+        LoadCompounds();
+        LoadSolubility();
+        
         PrintCompoundList();
     }
     void LoadSolvents()
     {
-        string jsonText = File.ReadAllText(solventsJsonFilePath);
-        SolventsCollection solventsCollection = JsonUtility.FromJson<SolventsCollection>(jsonText);
 
-        foreach (var solventData in solventsCollection.solvents)
-        {
-            SubstanceSolvent newSolvent = new SubstanceSolvent(
-                solventData.name,
-                solventData.color,
-                solventData.state,
-                solventData.density
-            );
-        
-            solvents[solventData.name] = newSolvent;
-        }
+    }    
+    void LoadCompounds()
+    {
+
     }
-
 
     void LoadChemicalData()
     {
-        string jsonText = File.ReadAllText(compoundsJsonFilePath);
-
-        Debug.Log(jsonText);
-
-        CompoundsCollection dataRoot = JsonUtility.FromJson<CompoundsCollection>(jsonText);
-
-        if (dataRoot.compounds == null)
-        {
-            Debug.Log("dataRoot é null ou dataRoot.compounds é null.");
-            return;
-        }
-
-        foreach (CompoundData data in dataRoot.compounds)
-        {
-            if (data.solubility == null)
-            {
-                Debug.Log($"data.solubility é null para o composto {data.compoundName}.");
-                continue;
-            }
-
-            Dictionary<SubstanceSolvent, string> solubilityTable = new Dictionary<SubstanceSolvent, string>();
-
-            foreach (var entry in data.solubility)
-            {
-                if (string.IsNullOrEmpty(entry.solvent) || string.IsNullOrEmpty(entry.value))
-                {
-                    Debug.Log($"entry.solvent ou entry.value é null ou vazio para o composto {data.compoundName}.");
-                    continue;
-                }
-
-                SubstanceSolvent solvent = GetSolventByName(entry.solvent);
-                if (solvent == null)
-                {
-                    Debug.Log($"Solvente {entry.solvent} retornou null para o composto {data.compoundName}.");
-                    continue;
-                }
-
-                solubilityTable[solvent] = entry.value;
-                if (!solvents.ContainsKey(entry.solvent))
-                {
-                    solvents[entry.solvent] = solvent;
-                }
-            }
-
-            PhysicalState state;
-            GroupName groupName;
-
-            try
-            {
-                state = (PhysicalState)Enum.Parse(typeof(PhysicalState), data.physicalState);
-                groupName = (GroupName)Enum.Parse(typeof(GroupName), data.groupName);
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError($"Erro ao fazer parse dos enums para o composto {data.compoundName}. Mensagem: {ex.Message}");
-                continue;
-            }
-
-            SubstanceCompound newCompound = new SubstanceCompound(
-                data.compoundName,
-                data.color,
-                state,
-                data.density,
-                groupName
-            );
-
-            substanceCompounds.Add(newCompound);
-        }
-    }
-    
-    public SubstanceSolvent GetSolventByName(string solventName)
-    {
-        if (solvents.TryGetValue(solventName, out var solvent))
-        {
-            return solvent;
-        }
-        else
-        {
-            Debug.LogError($"Solvente {solventName} não encontrado.");
-            return null;
-        }
+        
     }
 
     void PrintCompoundList()
---
+++ ChemistryLab/Assets/Scripts/SubstanceSolution.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubstanceSolution : MonoBehaviour
+{
+    private int id;
+    private SubstanceSolvent solvent;
+    private SubstanceCompound compound;
+    private string solutionName;
+    private Color color;
+    private PhysicalState state;
+    private float density;
+    private SolubilityResults solubilityResult;
+
+    public SubstanceSolution(int id, SubstanceSolvent solvent, SubstanceCompound compound, string solutionName, Color color, PhysicalState state, float density, SolubilityResults solubilityResult)
+    {
+        this.id = id;
+        this.solvent = solvent;
+        this.compound = compound;
+        this.solutionName = solutionName;
+        this.color = color;
+        this.state = state;
+        this.density = density;
+        this.solubilityResult = solubilityResult;
+    }
+
+}
